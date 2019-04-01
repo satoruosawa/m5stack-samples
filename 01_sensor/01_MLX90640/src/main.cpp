fbb81a36e746682d8b73c0a793b946b4aadb1d12
Satoru Osawa <osawa@takram.com>
2019-04-01 16:14:13 +0900

    Refactoring

--- 01_sensor/01_MLX90640/src/main.cpp
+++ 01_sensor/01_MLX90640/src/main.cpp
@@ -7,9 +7,9 @@
 
 boolean isConnected ();
 
-const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
-static float mlx90640To[768];
-paramsMLX90640 mlx90640;
+const byte MLX90640_ADDRESS = 0x33; //Default 7-bit unshifted address of the MLX90640
+static float MLX90640_TO[768]; // 32 * 24
+paramsMLX90640 MLX90640;
 
 void setup () {
   Wire.begin();
@@ -28,11 +28,11 @@ void setup () {
   //Get device parameters - We only have to do this once
   int status;
   uint16_t eeMLX90640[832];
-  status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+  status = MLX90640_DumpEE(MLX90640_ADDRESS, eeMLX90640);
   if (status != 0)
     Serial.println("Failed to load system parameters");
 
-  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+  status = MLX90640_ExtractParameters(eeMLX90640, &MLX90640);
   if (status != 0)
     Serial.println("Parameter extraction failed");
 
@@ -42,26 +42,26 @@ void setup () {
 void loop () {
   for (byte x = 0 ; x < 2 ; x++) {
     uint16_t mlx90640Frame[834];
-    int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
+    int status = MLX90640_GetFrameData(MLX90640_ADDRESS, mlx90640Frame);
     if (status < 0) {
       Serial.print("GetFrame Error: ");
       Serial.println(status);
     }
 
-    float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
-    float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+    float vdd = MLX90640_GetVdd(mlx90640Frame, &MLX90640);
+    float Ta = MLX90640_GetTa(mlx90640Frame, &MLX90640);
 
     float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
     float emissivity = 0.95;
 
-    MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
+    MLX90640_CalculateTo(mlx90640Frame, &MLX90640, emissivity, tr, MLX90640_TO);
   }
 
-  for (int x = 0 ; x < 10 ; x++) {
+  for (int x = 0 ; x < 30 ; x++) {
     Serial.print("Pixel ");
     Serial.print(x);
     Serial.print(": ");
-    Serial.print(mlx90640To[x], 2);
+    Serial.print(MLX90640_TO[x], 2);
     Serial.print("C");
     Serial.println();
   }
@@ -71,7 +71,7 @@ void loop () {
 
 //Returns true if the MLX90640 is detected on the I2C bus
 boolean isConnected () {
-  Wire.beginTransmission((uint8_t)MLX90640_address);
+  Wire.beginTransmission((uint8_t)MLX90640_ADDRESS);
   if (Wire.endTransmission() != 0) return false; //Sensor did not ACK
   return true;
 }
