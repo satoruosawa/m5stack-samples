30731a824793224065cd07268fb840e001564db1
Satoru Osawa <osawa@takram.com>
2019-04-18 18:23:53 +0900

    Add connection

---
+++ 02_communication/00_wifi/connection/src/main.cpp
@@ -0,0 +1,22 @@
+#include "./wifi-info.h"
+#include <M5Stack.h>
+#include <WiFi.h>
+
+const String SSID = WIFI_SSID; // "xxxx"
+const String PASSWORD = WIFI_PASSWORD; // "xxxx"
+
+void setup() {
+  M5.begin();
+  WiFi.begin(SSID.c_str(), PASSWORD.c_str());
+  M5.Lcd.printf("Connecting to the WiFi AP: %s ", SSID.c_str());
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    M5.Lcd.print(".");
+  }
+  M5.Lcd.println(" connected.");
+  M5.Lcd.print("IP address: ");
+  M5.Lcd.println(WiFi.localIP());
+}
+
+void loop() {
+}
