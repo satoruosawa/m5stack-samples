ef72454f29f7e3ca4c85d5d7dc1de1c7c44211b8
Satoru Osawa <osawa@takram.com>
2020-01-28 19:55:19 +0900

    Added Thingspeak

--- 04_iot-cloud/00_thingspeak/README.md
+++ 04_iot-cloud/00_thingspeak/README.md
@@ -1,10 +1,11 @@
 # ThingSpeakの設定
 [M5Stackで、においセンサー（TGS2450）を使ってみる。](https://magazine.halake.com/entry/m5stack-smell-sensor-thingspeak?utm_source=feed)
+[mathworks/thingspeak-arduino](https://github.com/mathworks/thingspeak-arduino)
 - Channelを作成
   - Channelsで New Channelをクリック
   - Nameを入力してsave
   - (設定は後からでも変更可能)
-- 
+- API Keys タブからAPI Keyを取得
 
 
 
@@ -23,6 +24,9 @@
 ```
 #ifndef API_INFO_H_
 #define API_INFO_H_
+
+const unsigned long CHANNEL_ID = 000000;
+const String API_KEY = "XXXXXXXXXXXXXXX";
 
 #endif
 ```
--- 04_iot-cloud/00_thingspeak/src/main.cpp
+++ 04_iot-cloud/00_thingspeak/src/main.cpp
@@ -1,13 +1,44 @@
+#include <HTTPClient.h>
 #include <M5Stack.h>
+#include <ThingSpeak.h>
 #include <WiFi.h>
 #include "./api-info.h"
 #include "./wifi-info.h"
 
+WiFiClient WIFI_CLIENT;
 const String SSID = WIFI_SSID;          // "xxxx"
 const String PASSWORD = WIFI_PASSWORD;  // "xxxx"
 
+void connectWifi();
+
 void setup() {
   M5.begin();
+  connectWifi();
+  ThingSpeak.begin(WIFI_CLIENT);
+}
+
+int cnt = 0;
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWifi();
+  }
+
+  ThingSpeak.setField(1, cnt);
+  int return_code = ThingSpeak.writeFields(CHANNEL_ID, API_KEY.c_str());
+
+  if (return_code == 200) {
+    Serial.println("Channel update successful.");
+  } else {
+    Serial.println("Problem updating channel. HTTP error code " +
+                   String(return_code));
+  }
+
+  cnt++;
+  delay(60000);
+}
+
+void connectWifi() {
   WiFi.begin(SSID.c_str(), PASSWORD.c_str());
   M5.Lcd.printf("Connecting to the WiFi AP: %s ", SSID.c_str());
   while (WiFi.status() != WL_CONNECTED) {
@@ -18,5 +49,3 @@ void setup() {
   M5.Lcd.print("IP address: ");
   M5.Lcd.println(WiFi.localIP());
 }
-
-void loop() {}
