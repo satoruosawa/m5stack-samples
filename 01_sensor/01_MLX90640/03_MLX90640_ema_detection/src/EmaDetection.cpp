92d2aa476025892647174614caee0c1d66013c34
Satoru Osawa <osawa@takram.com>
2020-01-28 17:40:00 +0900

    Refactoring sensors

--- 01_sensor/01_MLX90640/03_MLX90640_ema_detection/src/EmaDetection.cpp
+++ 01_sensor/01_MLX90640/03_MLX90640_ema_detection/src/EmaDetection.cpp
@@ -1,7 +1,7 @@
 #include "./EmaDetection.h"
 
-EmaDetection::EmaDetection ()
-: refEmaCoef(0.01), sampleEmaCoef(0.1), mlx90640Address(0x33) {
+EmaDetection::EmaDetection()
+    : refEmaCoef(0.01), sampleEmaCoef(0.1), mlx90640Address(0x33) {
   // Coefs should be below.
   // refEmaCoef < sampleEmaCoef
   rawValues = new float[VALUE_COUNT];
@@ -16,43 +16,50 @@ EmaDetection::EmaDetection ()
   }
 }
 
-void EmaDetection::Setup () {
+void EmaDetection::Setup() {
   Wire.begin();
-  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
+  Wire.setClock(400000);  // Increase I2C clock speed to 400kHz
   if (isConnected() == false) {
-    Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
-    while (1);
+    Serial.println(
+        "MLX90640 not detected at default I2C address. Please check wiring. "
+        "Freezing.");
+    while (1)
+      ;
   }
   Serial.println("MLX90640 online!");
 
   int status;
   uint16_t eeMLX90640[832];
   status = MLX90640_DumpEE(mlx90640Address, eeMLX90640);
-  if (status != 0)
-    Serial.println("Failed to load system parameters");
+  if (status != 0) Serial.println("Failed to load system parameters");
 
   status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
-  if (status != 0)
-    Serial.println("Parameter extraction failed");
+  if (status != 0) Serial.println("Parameter extraction failed");
 
-  //Set refresh rate
-  //A rate of 0.5Hz takes 4Sec per reading because we have to read two frames to get complete picture
-  //MLX90640_SetRefreshRate(mlx90640Address, 0x00); //Set rate to 0.25Hz effective - Works
-  //MLX90640_SetRefreshRate(mlx90640Address, 0x01); //Set rate to 0.5Hz effective - Works
-  //MLX90640_SetRefreshRate(mlx90640Address, 0x02); //Set rate to 1Hz effective - Works
-  //MLX90640_SetRefreshRate(mlx90640Address, 0x03); //Set rate to 2Hz effective - Works
-  // MLX90640_SetRefreshRate(mlx90640Address, 0x04); //Set rate to 4Hz effective - Works
-  MLX90640_SetRefreshRate(mlx90640Address, 0x05); //Set rate to 8Hz effective - Works at 800kHz
-  // MLX90640_SetRefreshRate(mlx90640Address, 0x06); //Set rate to 16Hz effective - Works at 800kHz
-  // MLX90640_SetRefreshRate(mlx90640Address, 0x07); //Set rate to 32Hz effective - fails
+  // Set refresh rate
+  // A rate of 0.5Hz takes 4Sec per reading because we have to read two frames
+  // to get complete picture MLX90640_SetRefreshRate(mlx90640Address, 0x00);
+  // //Set rate to 0.25Hz effective - Works
+  // MLX90640_SetRefreshRate(mlx90640Address, 0x01); //Set rate to 0.5Hz
+  // effective - Works MLX90640_SetRefreshRate(mlx90640Address, 0x02); //Set rate
+  // to 1Hz effective - Works MLX90640_SetRefreshRate(mlx90640Address, 0x03);
+  // //Set rate to 2Hz effective - Works
+  // MLX90640_SetRefreshRate(mlx90640Address, 0x04); //Set rate to 4Hz effective
+  // - Works
+  MLX90640_SetRefreshRate(mlx90640Address,
+                          0x05);  // Set rate to 8Hz effective - Works at 800kHz
+  // MLX90640_SetRefreshRate(mlx90640Address, 0x06); //Set rate to 16Hz
+  // effective - Works at 800kHz MLX90640_SetRefreshRate(mlx90640Address, 0x07);
+  // //Set rate to 32Hz effective - fails
 
-  //Once EEPROM has been read at 400kHz we can increase to 1MHz
-  Wire.setClock(1000000); //Teensy will now run I2C at 800kHz (because of clock division)
+  // Once EEPROM has been read at 400kHz we can increase to 1MHz
+  Wire.setClock(1000000);  // Teensy will now run I2C at 800kHz (because of
+                           // clock division)
   delay(1000);
   InitiateValues();
 }
 
-void EmaDetection::InitiateValues () {
+void EmaDetection::InitiateValues() {
   // XXX
   UpdateRaw();
   UpdateRaw();
@@ -63,29 +70,30 @@ void EmaDetection::InitiateValues () {
   }
 }
 
-void EmaDetection::Update () {
+void EmaDetection::Update() {
   UpdateRaw();
   UpdateRef();
   UpdateSample();
   UpdateDiff();
 }
 
-void EmaDetection::UpdateRaw () {
-  for (byte x = 0 ; x < 2 ; x++) {
+void EmaDetection::UpdateRaw() {
+  for (byte x = 0; x < 2; x++) {
     uint16_t mlx90640Frame[834];
     int status = MLX90640_GetFrameData(mlx90640Address, mlx90640Frame);
 
     float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
     float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
 
-    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+    float tr = Ta - TA_SHIFT;  // Reflected temperature based on the sensor
+                               // ambient temperature
     float emissivity = 0.95;
 
     MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, rawValues);
   }
 }
 
-void EmaDetection::UpdateRef () {
+void EmaDetection::UpdateRef() {
   for (int i = 0; i < VALUE_COUNT; i++) {
     double value = static_cast<double>(rawValues[i]);
     if (value < 1 || value > 100) return;
@@ -93,15 +101,16 @@ void EmaDetection::UpdateRef () {
   }
 }
 
-void EmaDetection::UpdateSample () {
+void EmaDetection::UpdateSample() {
   for (int i = 0; i < VALUE_COUNT; i++) {
     double value = static_cast<double>(rawValues[i]);
     if (value < 1 || value > 100) return;
-    sampleEmaValues[i] = DoubleLerp(0, sampleEmaValues[i], 1, value, sampleEmaCoef);
+    sampleEmaValues[i] =
+        DoubleLerp(0, sampleEmaValues[i], 1, value, sampleEmaCoef);
   }
 }
 
-void EmaDetection::UpdateDiff () {
+void EmaDetection::UpdateDiff() {
   float diffMin = 100;
   float diffMax = -100;
   for (int i = 0; i < VALUE_COUNT; i++) {
@@ -120,7 +129,7 @@ void EmaDetection::UpdateDiff () {
   }
 }
 
-void EmaDetection::Draw () {
+void EmaDetection::Draw() {
   for (int y = 0; y < 24; y++) {
     for (int x = 0; x < 32; x++) {
       int index = x + y * 32;
@@ -134,8 +143,8 @@ void EmaDetection::Draw () {
   }
 }
 
-boolean EmaDetection::isConnected () {
+boolean EmaDetection::isConnected() {
   Wire.beginTransmission((uint8_t)mlx90640Address);
-  if (Wire.endTransmission() != 0) return false; //Sensor did not ACK
+  if (Wire.endTransmission() != 0) return false;  // Sensor did not ACK
   return true;
 }
--- 01_sensor/01_MLX90640/03_MLX90640_ema_detection/src/EmaDetection.h
+++ 01_sensor/01_MLX90640/03_MLX90640_ema_detection/src/EmaDetection.h
@@ -1,38 +1,38 @@
 #ifndef EMA_DETECTION_H_
 #define EMA_DETECTION_H_
 
+#include <M5Stack.h>
+#include <Wire.h>
+#include "./Modules.h"
 #include "./library/MLX90640_API.h"
 #include "./library/MLX90640_I2C_Driver.h"
-#include "./Modules.h"
-#include <Wire.h>
-#include <M5Stack.h>
 
-#define TA_SHIFT 8 //Default shift for MLX90640 in open air
+#define TA_SHIFT 8  // Default shift for MLX90640 in open air
 #define X_COUNT 32
 #define Y_COUNT 24
-#define VALUE_COUNT 768 // 32 * 24
+#define VALUE_COUNT 768  // 32 * 24
 
 class EmaDetection {
-public:
-  EmaDetection ();
-  void Setup ();
-  void Update ();
-  void Draw ();
+ public:
+  EmaDetection();
+  void Setup();
+  void Update();
+  void Draw();
 
-private:
-  void InitiateValues ();
-  void UpdateRaw ();
-  void UpdateRef ();
-  void UpdateSample ();
-  void UpdateDiff ();
-  boolean isConnected ();
-  const byte mlx90640Address;
-  float * rawValues;
-  double * refEmaValues;
-  double * sampleEmaValues;
-  double * diffValues;
+ private:
+  void InitiateValues();
+  void UpdateRaw();
+  void UpdateRef();
+  void UpdateSample();
+  void UpdateDiff();
+  boolean isConnected();
+  float* rawValues;
+  double* refEmaValues;
+  double* sampleEmaValues;
+  double* diffValues;
   const double refEmaCoef;
   const double sampleEmaCoef;
+  const byte mlx90640Address;
   paramsMLX90640 mlx90640;
   int diffMinIndex;
   int diffMaxIndex;
