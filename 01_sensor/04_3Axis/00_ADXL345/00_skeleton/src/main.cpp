1da148eb7821f144adedbf56339feae6c4ba5ede
Satoru Osawa <osawa@takram.com>
2020-03-16 00:10:07 +0900

    Added Simple ADXL345

--- 01_sensor/04_3Axis/00_ADXL345/00_skeleton/src/main.cpp
+++ 01_sensor/04_3Axis/00_ADXL345/00_skeleton/src/main.cpp
@@ -1,9 +1,22 @@
-#include <Arduino.h>
+#include <ADXL345.h>
+#include <M5Stack.h>
+
+ADXL345 adxl;
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin();
+  adxl.powerOn();
+  dacWrite(25, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int x, y, z;
+  adxl.readXYZ(&x, &y, &z);
+  Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);
+  delay(10);
 }
