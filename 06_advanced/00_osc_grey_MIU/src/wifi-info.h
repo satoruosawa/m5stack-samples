19c676e315ea78f266a0f1f10a5843808e7d678d
Satoru Osawa <osawa@takram.com>
2020-04-14 22:27:08 +0900

    Add osc grey MIU

---
+++ 06_advanced/00_osc_grey_MIU/src/wifi-info.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_INFO_H_
+#define WIFI_INFO_H_
+
+// #define WIFI_SSID "takram-guest"
+// #define WIFI_PASSWORD "welcometakram"
+
+#define WIFI_SSID "AirPortSTR"
+#define WIFI_PASSWORD "Gvx4HCaP7D"
+
+#endif  // WIFI_INFO_H_
