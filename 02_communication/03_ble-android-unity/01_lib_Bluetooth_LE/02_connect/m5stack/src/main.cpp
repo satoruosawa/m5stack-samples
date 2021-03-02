f03dba1a4a6bf82923c8724688b0fce6c44a9c8c
Satoru Osawa <osawa@takram.com>
2021-03-02 19:36:04 +0900

    Refactoring

--- 02_communication/03_ble-android-unity/01_lib_Bluetooth_LE/02_connect/m5stack/src/main.cpp
+++ 02_communication/03_ble-android-unity/01_lib_Bluetooth_LE/02_connect/m5stack/src/main.cpp
@@ -2,9 +2,6 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <M5Stack.h>
-#include <Wire.h>
-#include <esp_gatts_api.h>
-#include <esp_wifi.h>
 
 #define SERVICE_UUID "00002220-0000-1000-8000-00805F9B34FB"
 #define CHARACTERISTIC_UUID "00002221-0000-1000-8000-00805F9B34FB"
@@ -12,56 +9,65 @@
 BLEServer* pServer = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
+std::__cxx11::string receivedValue = "";
 
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
+    // The program stops if you call M5.lcd.print function in the callback.
     Serial.println("connect");
     deviceConnected = true;
   };
   void onDisconnect(BLEServer* pServer) {
+    // The program stops if you call M5.lcd.print function in the callback.
     Serial.println("disconnect");
     deviceConnected = false;
   }
 };
 
 class dataCb : public BLECharacteristicCallbacks {
-  void onRead(BLECharacteristic* pCharacteristic) { Serial.println("onRead"); }
+  void onRead(BLECharacteristic* pCharacteristic) {
+    // The program stops if you call M5.lcd.print function in the callback.
+    Serial.println("onRead");
+  }
   void onWrite(BLECharacteristic* pCharacteristic) {
+    // The program stops if you call M5.lcd.print function in the callback.
     Serial.println("onWrite");
-    Serial.println(pCharacteristic->getValue().c_str());
+    receivedValue = pCharacteristic->getValue();
+    Serial.println(receivedValue.c_str());
   }
 };
 
 void setup() {
   M5.begin();
-  dacWrite(25, 0);  // Speaker OFF
-
   BLEDevice::init("M5Stack");
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
   BLEService* pService = pServer->createService(SERVICE_UUID);
   pService
       ->createCharacteristic(
           CHARACTERISTIC_UUID,
           BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE)
       ->setCallbacks(new dataCb());
-
   pService->start();
-  pServer->getAdvertising()->start();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  pServer->startAdvertising();
+  Serial.println("Start advertising...  ");
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("BLE Read Write");
+  M5.Lcd.setCursor(0, 18);
+  M5.Lcd.println("Start advertising...  ");
 }
 
 void loop() {
   if (deviceConnected) {
-    //
+    M5.Lcd.println("                      ");
   }
   if (!deviceConnected && oldDeviceConnected) {
     delay(500);
-    pServer->getAdvertising()->start();
-    Serial.println("start advertising");
+    pServer->startAdvertising();
+    Serial.println("Restart advertising");
     M5.Lcd.setCursor(0, 18);
-    M5.Lcd.println("Advertising..");
+    M5.Lcd.println("Restart advertising...");
     oldDeviceConnected = deviceConnected;
   }
   if (deviceConnected && !oldDeviceConnected) {
