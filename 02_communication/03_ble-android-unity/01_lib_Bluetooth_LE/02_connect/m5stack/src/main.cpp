52283df6ec33386c309924b0ce59cecf77421906
Satoru Osawa <osawa@takram.com>
2021-03-02 18:10:49 +0900

    Add read callback

---
+++ 02_communication/03_ble-android-unity/01_lib_Bluetooth_LE/02_connect/m5stack/src/main.cpp
@@ -0,0 +1,59 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <M5Stack.h>
+#include <Wire.h>
+#include <esp_gatts_api.h>
+#include <esp_wifi.h>
+
+#define SERVICE_UUID "00002220-0000-1000-8000-00805F9B34FB"
+#define CHARACTERISTIC_UUID "00002221-0000-1000-8000-00805F9B34FB"
+
+BLEServer* pServer = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) { deviceConnected = true; };
+  void onDisconnect(BLEServer* pServer) { deviceConnected = false; }
+};
+
+class dataCb : public BLECharacteristicCallbacks {
+  void onRead(BLECharacteristic* pChar) { Serial.println("Get Data"); }
+};
+
+void setup() {
+  M5.begin();
+  dacWrite(25, 0);  // Speaker OFF
+
+  BLEDevice::init("M5Stack");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService* pService = pServer->createService(SERVICE_UUID);
+  pService
+      ->createCharacteristic(CHARACTERISTIC_UUID,
+                             BLECharacteristic::PROPERTY_READ)
+      ->setCallbacks(new dataCb());
+
+  pService->start();
+  pServer->getAdvertising()->start();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  if (deviceConnected) {
+    //
+  }
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500);
+    pServer->getAdvertising()->start();
+    Serial.println("start advertising");
+    M5.Lcd.setCursor(0, 18);
+    M5.Lcd.println("Advertising..");
+    oldDeviceConnected = deviceConnected;
+  }
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+  }
+}
